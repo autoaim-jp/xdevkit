e884a56db71942d42c6d99355b29aec4b04f6c4f
autoaim_jp <git@autoaim.jp>
2022-07-25 19:33:35 +0900

    remove: xdevkitConstant

--- core/apiRouter.js
+++ core/apiRouter.js
@@ -1,22 +1,6 @@
 /* /xdevkit/core/apiRouter.js */
 const mod = {}
 
-const XLOGIN_REDIRECT_URI = encodeURIComponent('/f/xlogin/callback')
-const scope = 'r:emailAddress,*r:userName,*r:serviceUserId'
-const filterKeyList = ['emailAddress', 'userName', 'serviceUserId']
-
-/* xdevkit common constant */
-const xdevkitConstant = {}
-xdevkitConstant.API_VERSION = 'v0.7'
-xdevkitConstant.STATE_L = 64
-xdevkitConstant.CODE_VERIFIER_L = 64
-xdevkitConstant.XLOGIN_RESPONSE_TYPE = 'code'
-xdevkitConstant.XLOGIN_CODE_CHALLENGE_METHOD = 'S256'
-
-xdevkitConstant.XLOGIN_AUTHORIZATION_ENDPOINT = `/api/${xdevkitConstant.API_VERSION}/auth/connect`
-xdevkitConstant.XLOGIN_CODE_ENDPOINT = `/api/${xdevkitConstant.API_VERSION}/auth/code`
-xdevkitConstant.XLOGIN_USER_INFO_ENDPOINT = `/api/${xdevkitConstant.API_VERSION}/user/info`
-
 const init = (browserServerSetting, setting, lib, express) => {
   mod.bsc = browserServerSetting
   mod.setting = setting
@@ -41,20 +25,20 @@ const _getErrorResponse = (status, error, isServerRedirect, response = null, ses
 const _handleXloginConnect = (redirectAfterAuth) => {
   const oidcSessionPart = {}
   oidcSessionPart['iss'] = mod.setting.env.AUTH_SERVER_ORIGIN
-  oidcSessionPart['codeVerifier'] = mod.lib.getRandomB64UrlSafe(xdevkitConstant.CODE_VERIFIER_L)
+  oidcSessionPart['codeVerifier'] = mod.lib.getRandomB64UrlSafe(mod.setting.api.CODE_VERIFIER_L)
   oidcSessionPart['redirectAfterAuth'] = redirectAfterAuth
 
   const oidcQueryParam = {}
-  oidcQueryParam['codeChallengeMethod'] = xdevkitConstant.XLOGIN_CODE_CHALLENGE_METHOD
+  oidcQueryParam['codeChallengeMethod'] = mod.setting.api.XLOGIN_CODE_CHALLENGE_METHOD
   oidcQueryParam['codeChallenge'] = mod.lib.convertToCodeChallenge(oidcSessionPart['codeVerifier'], oidcQueryParam['codeChallengeMethod'])
-  oidcQueryParam['state'] = mod.lib.getRandomB64UrlSafe(xdevkitConstant.STATE_L)
-  oidcQueryParam['responseType'] = xdevkitConstant.XLOGIN_RESPONSE_TYPE 
-  oidcQueryParam['scope'] = scope
+  oidcQueryParam['state'] = mod.lib.getRandomB64UrlSafe(mod.setting.api.STATE_L)
+  oidcQueryParam['responseType'] = mod.setting.api.XLOGIN_RESPONSE_TYPE 
+  oidcQueryParam['scope'] = mod.setting.api.SCOPE
   oidcQueryParam['clientId'] = mod.setting.env.CLIENT_ID
-  oidcQueryParam['redirectUri'] = mod.setting.env.SERVER_ORIGIN + XLOGIN_REDIRECT_URI
+  oidcQueryParam['redirectUri'] = mod.setting.env.SERVER_ORIGIN + mod.setting.url.XLOGIN_REDIRECT_URI
 
-  const oidcQueryStr = mod.lib.objToQuery(oidcQueryParam) 
-  const redirectTo = `${mod.setting.env.AUTH_SERVER_ORIGIN}${xdevkitConstant.XLOGIN_AUTHORIZATION_ENDPOINT}?${oidcQueryStr}`
+  const oidcQueryStr = `?${mod.lib.objToQuery(oidcQueryParam)}`
+  const redirectTo = mod.setting.env.AUTH_SERVER_ORIGIN + mod.setting.url.XLOGIN_AUTHORIZATION_ENDPOINT + oidcQueryStr
 
   const newUserSession = { oidc: Object.assign(oidcSessionPart, oidcQueryParam) }
 
@@ -83,7 +67,7 @@ const _handleXloginCode = async (state, code, iss, userSession) => {
   }
 
   /* request accessToken */
-  const accessTokenResponse = await mod.lib.getAccessTokenByCode(mod.lib.apiRequest, code, userSession.oidc, mod.setting.env.AUTH_SERVER_ORIGIN + xdevkitConstant.XLOGIN_CODE_ENDPOINT)
+  const accessTokenResponse = await mod.lib.getAccessTokenByCode(mod.lib.apiRequest, code, userSession.oidc, mod.setting.env.AUTH_SERVER_ORIGIN + mod.setting.url.XLOGIN_CODE_ENDPOINT)
   if (!accessTokenResponse) {
     const status = mod.bsc.statusList.INVALID_SESSION
     const error = 'handle_xlogin_code_access_token'
@@ -98,7 +82,7 @@ const _handleXloginCode = async (state, code, iss, userSession) => {
   }
 
   /* request userInfo */
-  const userInfoResponse = await mod.lib.getUserInfo(mod.lib.apiRequest, mod.setting.env.CLIENT_ID, filterKeyList, accessToken, mod.setting.env.AUTH_SERVER_ORIGIN + xdevkitConstant.XLOGIN_USER_INFO_ENDPOINT)
+  const userInfoResponse = await mod.lib.getUserInfo(mod.lib.apiRequest, mod.setting.env.CLIENT_ID, mod.setting.api.FILTER_KEY_LIST, accessToken, mod.setting.env.AUTH_SERVER_ORIGIN + mod.setting.url.XLOGIN_USER_INFO_ENDPOINT)
   if (!userInfoResponse) {
     const status = mod.bsc.statusList.INVALID_SESSION
     const error = 'handle_xlogin_code_user_info'
